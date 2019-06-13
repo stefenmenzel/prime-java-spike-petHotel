362756dc5c1f02fed0746f0efd7ae12d9ffa45e4
Madeline Morton <madelinemorton614@gmail.com>
2019-06-13 10:42:12 -0500

    updated account info

--- src/redux/sagas/petSaga.js
+++ src/redux/sagas/petSaga.js
@@ -1,23 +1,23 @@
 
-// // ===== PET SAGA ===== //
-// import {put, takeLatest} from 'redux-saga/effects';
-// import axios from 'axios';
+// ===== PET SAGA ===== //
+import {put, takeLatest} from 'redux-saga/effects';
+import axios from 'axios';
 
-// // Adds new pet to the DB
-// function* addPet(action) {
-//     try {
-//         const petResponse = yield axios.post(`/addPet`, action.payload);
-//         console.log('addPet Response', petResponse.data);
-//         yield put({ type: 'FETCH_PETS', payload: petResponse.data })
-//     } catch (error) {
-//         console.log('error in postName', error )
-//     }
-// } // end addPet Saga
+// Adds new pet to the DB
+function* addPet(action) {
+    try {
+        const petResponse = yield axios.post(`/addPet`, action.payload);
+        console.log('addPet Response', petResponse.data);
+        yield put({ type: 'FETCH_PETS', payload: petResponse.data })
+    } catch (error) {
+        console.log('error in postName', error )
+    }
+} // end addPet Saga
 
 
-// // Watcher Saga
-// function* petSaga() {
-//     yield takeLatest('ADD_PET', addPet)
-// } // end Watcher Saga petSaga
+// Watcher Saga
+function* petSaga() {
+    yield takeLatest('ADD_PET', addPet)
+} // end Watcher Saga petSaga
 
-// export default petSaga;
+export default petSaga;
